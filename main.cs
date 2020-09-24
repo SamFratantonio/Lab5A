f57140a0464dbd556596e06f326835e99f73dd93
SamFratantonio <fratantonios04@gmail.com>
2020-09-24 17:23:19 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,18 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    double sAverage = 0;
+    for (int x = 1; x <= 7; x++)
+    {
+      Console.Write("Enter The Distance Covered By Car " + x + ": ");
+      double d = double.Parse(Console.ReadLine());
+      Console.Write("Enter The Time Taken By Car " + x + ": ");
+      double t = double.Parse(Console.ReadLine());
+      Console.WriteLine("The Speed Of Car " + x + " is " 
+      + (d / t) + " Miles Per Hour");
+      sAverage += (d / t);
+    }
+    Console.WriteLine("The Average Speed Is " + sAverage + " Miles Per Hour");
+  }
+}
